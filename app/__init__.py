81fe05a11103759acdc516cdb2152854f4b8f799
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-11-12 20:24:48 -0500

    finished p4

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from app.config import Config
 import jinja2
 from app.tweets import tweets
 import random
-
+from app.form.form import Form
 app = Flask(__name__)
 app.config.from_object(Config)
+from datetime import datetime
+
 
 @app.route("/")
 def index():
@@ -15,5 +17,21 @@ def index():
 
 @app.route("/feed")
 def feed():
+
     return render_template('feed.html', tweets=tweets)
 
+@app.route("/new", methods=["GET", "POST"])
+def new():
+    form = Form()
+
+    if form.validate_on_submit():
+        print(form.data)
+        newTweet = {
+            "id": len(tweets) + 1,
+            "author": form.author.data,
+            "date" : datetime.now().strftime("%m/%d/%Y") ,
+            "tweet": form.tweet.data,
+            "likes": 0,
+        }
+        tweets.append(newTweet)
+    return render_template('new.html', form=form)
